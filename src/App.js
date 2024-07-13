eed0ffd872557f7a42c023066ac0154a68962895
riddhesh710 <riddhesh710@gmail.com>
2024-07-14 03:29:55 +0530

    Checkpoint: Decent looking UI without session

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import ChatBox from './components/ChatBox';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="app">
-      <Sidebar />
-      <ChatBox />
+    <div className="outer-app">
+      <div className="header-container">
+        <Header  />
+      </div>
+      <div className='app'>
+        <Sidebar />
+        <ChatBox />
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,12 @@
-// src/components/Header.js
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
 const Header = () => {
-  const logoUrl = 'https://insightbotadls.blob.core.windows.net/sitecontent/onlylion.png'; // Replace with your actual logo URL
-
   return (
-    <AppBar position="static" sx={{ bgcolor: 'primary.main' }}>
-      <Toolbar>
-        <Box display="flex" justifyContent="center" alignItems="center" width="100%">
-          <Box display="flex" alignItems="center">
-            <img src={logoUrl} alt="Logo" style={{ height: '60px', marginRight: '10px' }} />
-            <Typography variant="h5">
-                <b>InightBOT</b>
-            </Typography>
-          </Box>
-        </Box>
-      </Toolbar>
-    </AppBar>
+    <div className="header">
+      <img src="https://insightbotadls.blob.core.windows.net/sitecontent/onlylion.png" alt="Organization Logo" className="logo" />
+      <h1 className="organization-name">CYBERBHARAT ANALYTICS</h1>
+    </div>
   );
-};
+}
 
 export default Header;
